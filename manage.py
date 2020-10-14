2d6b06508a6e5cac6c68f61b01c02ae4bc1e0fc3
Sr. Developer <danielatupwork@gmail.com>
2020-10-14 00:32:31 -0700

    Update manage.py

--- manage.py
+++ manage.py
@@ -20,3 +20,35 @@ if __name__ == "__main__":
             )
         raise
     execute_from_command_line(sys.argv)
+
+# 1. 1915594740(riunsim)
+# Hi, I am fine here, everything goes well. I really missing you.
+# I heard the news from boss, what I want to favor to you is that keep take care my mother untill I came back to homeland even if you are hard to live.
+# I will send $300 via gyongye's mother, use for mother's health.
+# I really want to see my living mother when I am in homeland.
+# I will contact you twice a month, let me know mother's health status and problems that are troubling you.
+# Boss should not be aware of this contact and money problem.
+# I belive you and love you. Please take care my mother well.
+# 
+# 2. 1915564411, 023391427, gyongye's mother
+# Hi, my love. I am fine here, everything goes well.
+# I really missing you and daughter, especially nowadays, I meet you almost everyday in my dream, so I hate waking up from a fantasic dream. So does you?
+# First, call hyonho's wife(riunsim) and give her $300 asap, because hyonho's mother is in cancer.
+# I be paid it here, and will send to you soon, but I don't know exactly when it will be delivered to you.
+# Because money is also quarantined in homeland. Just wait the call.
+# Second, The news of hyonho's mother made me worried about parent's health, especially your mother's health.
+# We have to be loyal to them when they are alive.
+# So you must take your mother to central hospital asap and check her health. If you do not until my next contact, I will be disappointed to you.
+# If anything happens in checking, don't hide it to me, you must let me know, my mental is strong.
+# And of course I think you're already going to act that way, send some money to my parents periodically.
+# My last favor to you is that take care yourself and make daughter also, your and daughter's health is my top priority interests.
+# I will also manage my health well. I hope we can meet in good health after corona.
+# As I said before, don't hesitate to spend money for health, I will do my best here. Believe me.
+# ps: Ask grettings to my brother and cousins, and let me know their news in next call if you can.
+# also you should get the news of YongBong's house, and let me know the result.
+# From your loving husband.
+# 
+# Hi, bro.
+# This is urgent call, mother of my one member is in dagerous cancer, and they need money asap, so please do it asap.
+# I know it's long history, but please don't miss any sentence so that our minds can be delivered to wifes.
+# Thanks always.
